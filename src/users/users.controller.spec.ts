6ba407dcf51750404f21567468589d838b0f7dd0
Lobo-rio <gsilvamedeiros@gmail.com>
2022-12-12 13:19:57 -0300

    feat: Implementation test controller

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,18 +1,32 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
 
 describe('UsersController', () => {
-  let controller: UsersController;
+  let usersController: UsersController;
+  let usersService: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: [{
+        provide: UsersService,
+        useValue: {
+          findAll: jest.fn(),
+          findById: jest.fn(),
+          create: jest.fn(),
+          update: jest.fn(),
+          remove: jest.fn(),
+        }
+      }],
     }).compile();
 
-    controller = module.get<UsersController>(UsersController);
+    usersController = module.get<UsersController>(UsersController);
+    usersService = module.get<UsersService>(UsersService);
   });
 
   it('should be defined', () => {
-    expect(controller).toBeDefined();
+    expect(usersController).toBeDefined();
+    expect(usersService).toBeDefined();
   });
 });
